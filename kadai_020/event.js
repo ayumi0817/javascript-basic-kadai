3ac5271b7c264e5173604a2d5e70d803bd1b5a70
ayumi0817 <jinpo.ayumi@gmail.com>
2023-12-03 14:06:56 +0900

    コミット

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+
+const Btnn = document.getElementById("btn")
+const Textt = document.getElementById("text")
+
+Btnn.addEventListener("click", ()=> {
+    const text2=document.getElementById("text");
+    text2.textContent = 'ボタンがクリックされました';
+
+})
+
